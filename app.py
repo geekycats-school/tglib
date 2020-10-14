289c17ff7ba78ff024c49e0d86e9ff459deacfa4
Mark Chigrin <ilmpc@ya.ru>
2020-10-14 12:25:05 +0000

    First version without requirements

--- app.py
+++ app.py
@@ -1,3 +1,17 @@
-def main():
-    pass
+import telegram
+import logging
 
+
+def main():
+    while True:
+        try:
+            chat_id, message = telegram.get_message()
+        except TimeoutError:
+            continue
+        except telegram.TelegramError as e:
+            logging.error(f"Exception occured\n{e}")
+            continue
+        parser = telegram.get_message_parser(chat_id, message)
+        telegram.send_message(chat_id, parser(message))
+
+main()
--- telegram.py
+++ telegram.py
@@ -1,26 +1,55 @@
 import requests
 import json
+import tasks
+from typing import List, Callable, Tuple
 
-TOKEN = ""
+TOKEN = "1360917608:AAGB4YtRDmIi7ywCMFb6IILGswf0wHCx-r0"
 URL = "https://api.telegram.org/bot{token}/{method}"
+_stored_chats = dict() #chat_id function
 
-def find_command(command_name: str, args: str) -> Function[str]:
+
+class TelegramError(Exception):
+    pass
+
+
+def get_command_parser(chat_id: int, command_name: str, *args: List[str]) -> Callable[[], str]:
     # /command arg1 arg2  
-    pass
+    if command_name == "new_task":
+        task, answer = tasks.get_task()
+        _stored_chats[chat_id] = lambda s: "Right!" if s.strip() == answer else "That isn't a right answer."
+        return lambda *args: task 
 
-def parse_message():
-    pass
+def get_message_parser(chat_id: int,  text: str) -> Callable[[],str]:
+    if text.lstrip().startswith("/"):
+        get_command_parser(chat_id, *text.split())
+    elif chat_id in _stored_chats:
+        return _stored_chats['chat_id']
+    else:
+        return lambda s: "No handler for your request"  
+      
 
-def get_message(offset: int, limit: int = 1) -> Tuple[int, str]:
+def get_message(offset: List[int] = [], limit: int = 1) -> Tuple[int, str]:
     # -> chat_id, text
-    pass
+    params = {
+        "offset" : offset[0] if len(offset) == 1 else 0,
+        "timeout" : 60
+    }
+    resp = requests.get(URL.format(token=TOKEN, method="getUpdates"), params=params, timeout=60)
+    if resp.ok and resp.json()['ok'] == True and len(resp.json()['result']) == 1:
+        result = resp.json()['result'][0]
+        offset[0] = result['update_id'] + 1
+        message = result['message']
+        return message['chat']['id'], message['text']
+    else:
+        raise TelegramError(resp.text)
+
 
 def send_message(chat_id: int, text: str):
     data = {
-        "chat_id": id,
+        "chat_id": chat_id,
         "text": text
     }
-    p = requests.post(URL.format(token=TOKEN, method="sendMessage"), data=data)
+    requests.post(URL.format(token=TOKEN, method="sendMessage"), data=data)
 
 
 
