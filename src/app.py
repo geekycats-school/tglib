314bc3324304b3eba9ee3df2cb4cfa90a2c33f42
grumpyLemons <mark.gulev@gmail.com>
2020-10-17 13:38:07 +0000

    refactoring

--- src/app.py
+++ src/app.py
@@ -6,30 +6,32 @@ from typing import Any, List, Callable
 _stored_chats = dict()  # chat_id answer
 
 
-def check_answer(chat_id: int, text: str) -> Callable[[], str]:
-    _, answer = tasks.get_task()
-    if text == answer:
-        return _stored_chats.pop(chat_id)
+def handler(chat_id, text):
+    if text.lstrip().startswith("/"):
+        pass
+
+def check_answer(chat_id: int, text: str) -> str:
+    if _stored_chats[chat_id] == text:
+        del _stored_chats[chat_id]
+        return "Correct"
     else:
-        return _stored_chats[chat_id]
+        return "Wrong"
 
 
 def get_command_parser(chat_id: int, command_name: str, *args: List[str]) -> Callable[[Any], str]:
-    # /command arg1 arg2  
     if command_name == "/new_task":
         task, answer = tasks.get_task()
         _stored_chats[chat_id] = answer
-        return lambda *args: task
+        return lambda s: task
     else:
         raise telegram.TelegramError
 
 
-def get_message_parser(chat_id: int, text: str) -> Callable[[], str]:
+def get_message_parser(chat_id: int, text: str) -> Callable[[str], str]:
     if text.lstrip().startswith("/"):
         return get_command_parser(chat_id, *text.split())
     elif chat_id in _stored_chats:
         return check_answer(chat_id, text)
-        # return _stored_chats.pop(chat_id)
     else:
         return lambda s: "No handler for your request"
 
