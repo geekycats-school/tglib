2c6b9ee3125bf4fc3a08a46cfb3434eefbb2426f
grumpyLemons <mark.gulev@gmail.com>
2020-10-18 04:33:08 +0000

    made handler function

--- src/app.py
+++ src/app.py
@@ -8,7 +8,16 @@ _stored_chats = dict()  # chat_id answer
 
 def handler(chat_id, text):
     if text.lstrip().startswith("/"):
-        pass
+        if text == "/new_task":
+            task, answer = tasks.get_task()
+            _stored_chats[chat_id] = answer
+            return task
+        else:
+            return "No handler for your request."
+    elif chat_id in _stored_chats:
+        return check_answer(chat_id, text)
+    else:
+        return "no handler for your request"
 
 def check_answer(chat_id: int, text: str) -> str:
     if _stored_chats[chat_id] == text:
@@ -40,8 +49,8 @@ def main():
     while True:
         try:
             chat_id, message = telegram.get_message()
-            parser = get_message_parser(chat_id, message)
-            telegram.send_message(chat_id, parser(message))
+            output = handler(chat_id, message)
+            telegram.send_message(chat_id, output)
         except TimeoutError:
             continue
         except telegram.TelegramError as e:
