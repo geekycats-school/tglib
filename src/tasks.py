dfea0453da066f812cf204f15aa23c561f906841
grumpyLemons <mark.gulev@gmail.com>
2020-11-21 13:29:57 +0000

    remade a Task class

--- src/tasks.py
+++ src/tasks.py
@@ -1,21 +1,36 @@
-  
 from typing import Tuple
 import random
 
-k_tpl = tuple([n for n in range(-10, 11) if n != 0])
+k_tpl = tuple(n for n in range(-10, 11) if n != 0)
 
 class Task:
-    def get_linear(self) -> Tuple[str, str]:
+
+    def __init__(self, task, answers_):
+        self.task = task
+        self.answers = answers_
+
+    def __repr__(self):
+        return self.out
+
+    def check_asnwer(self, answers):
+        if set(int(i) for i in answers.split()) == self.answers:
+            return True
+        return False
+
+    @staticmethod
+    def get_linear():
         x = random.randint(-50, 50)
         a = random.randint(0, 100)
         k = random.choice(k_tpl)
         b = a + (k * x)
         sign = "+" if k >= 0 else ""
         equation = f"{str(a)+f'{sign}' if a != 0 else ''}{k if abs(k) != 1 else ''}x={b}"
-        return (equation, str(x))
+        
+        return Task(equation, set(x))
 
 
-    def get_quadratic(self) -> Tuple[str, str]:
+    @staticmethod
+    def get_quadratic():
         a = random.randint(1, 10)
         x1 = random.randint(-10, 10)
         x2 = random.randint(-10, 10)
@@ -25,9 +40,4 @@ class Task:
         c_sign = "+" if c > 0 else ""
 
         equation = f"{a if a != 1 else ''}x\u00B2{f'{b_sign}'+str(b)+'x' if b != 0 else ''}{f'{c_sign}'+str(c) if c != 0 else ''}=0"
-        return (equation, "{} {}".format(*sorted((x1, x2))))
-
-    def __init__(self, t=0):
-        self.task = (self.get_linear, self.get_quadratic)[t]
-
-    
+        return Task(equation, set(str(x1), str(x2)))
